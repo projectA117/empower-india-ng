ce40c992f35945f8d5518ad2c6c06626cb83585d
adrsrinivas096 <adrsrinivas096@gmail.com>
2025-02-01 23:06:29 +0530

    git first commit
    git first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { VisionComponent } from './vision/vision.component';
+import { VendordsComponent } from './vendords/vendords.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SponsorsComponent } from './sponsors/sponsors.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'about-us', component: AboutComponent },
+    { path: 'vision', component: VisionComponent },
+    { path: 'vendors', component: VendordsComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'sponsors', component: SponsorsComponent },
+    { path: 'home', component: HomeComponent}
+];
---
+++ src/app/vendords/vendords.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vendords',
+  standalone: true,
+  imports: [],
+  templateUrl: './vendords.component.html',
+  styleUrl: './vendords.component.scss'
+})
+export class VendordsComponent {
+
+}
