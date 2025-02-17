8727e25f51569a37c62582af5ed640748e377dc8
adrsrinivas096 <adrsrinivas096@gmail.com>
2025-02-17 23:21:25 +0530

    Project details updated

---
+++ src/app/project-details/project-signoff/project-signoff.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-signoff',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-signoff.component.html',
+  styleUrl: './project-signoff.component.scss',
+})
+export class ProjectsignoffComponent {}
