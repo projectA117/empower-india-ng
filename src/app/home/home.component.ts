54531b1e4869e446ee647b5f23d7b74d5d6585a3
Gopi Krishna <gums@Gopis-MacBook-Pro.local>
2025-02-27 01:01:09 +0530

    home page css

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '@service/productservice';
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  providers: [ProductService],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  products: any | undefined;
 
+  responsiveOptions: any[] | undefined;
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.productService.getProductsSmall().then((products) => {
+      this.products = products;
+    });
+
+    this.responsiveOptions = [
+      {
+        breakpoint: '1199px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '991px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+      {
+        breakpoint: '767px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+    ];
+  }
+
+  getSeverity(status: string) {
+    switch (status) {
+      case 'INSTOCK':
+        return 'success';
+      case 'LOWSTOCK':
+        return 'warning';
+      case 'OUTOFSTOCK':
+        return 'danger';
+    }
+  }
 }
