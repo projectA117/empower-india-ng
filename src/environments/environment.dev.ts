01cc20706bab63eb2aeeec903688a2172943bff0
Balaraju Kadukuntla <balaraju004@gmail.com>
2025-04-18 13:32:55 -0400

    Update environment.dev.ts
    Resolve merge conflicts

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl:
-    'http://localhost:8080/empower_andhra/api/v1',
+    'https://empowerindia-api-dyckeafkc0ebhxft.southeastasia-01.azurewebsites.net/empower_andhra/api/v1',
   version: '0.0.9',
 };
