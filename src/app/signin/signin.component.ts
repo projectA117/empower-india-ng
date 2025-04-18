f5b0f64ebec21182f7fe577aa3b0f0b1598f0556
adrsrinivas096 <adrsrinivas096@gmail.com>
2025-04-19 01:41:59 +0530

    Token updated
    Token updated

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -65,7 +65,7 @@ export class SigninComponent implements OnInit {
       if (data) {
         this.signinForm.reset();
         if (data.id) {
-          sessionStorage.setItem('token', JSON.stringify(data.token));
+          sessionStorage.setItem('token', JSON.stringify(data.jwtToken));
           this.router.navigate(['/home']);
         } else {
           this.invalidUserNameOrPassword = true;
--- src/interceptors/auth.interceptor.ts
+++ src/interceptors/auth.interceptor.ts
@@ -1,11 +1,12 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = sessionStorage.getItem('token') ?? '156789';
+  const token = sessionStorage.getItem('token');
 
-  if (token) {
+  if (token && req.url.indexOf('googleapis') === -1) {
+    console.log(req.url);
     const authReq = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${token}`),
+      headers: req.headers.set('Authorization', `${token}`),
     });
     return next(authReq);
   }
