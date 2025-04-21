23ea01827a2bc84994b1e8867183e95279e833af
adrsrinivas096 <adrsrinivas096@gmail.com>
2025-04-21 23:28:51 +0530

    UI changes updated
    UI changes updated

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,6 +9,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { CommonService } from '@service/common.service';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-contact',
@@ -16,11 +17,13 @@ import { CommonService } from '@service/common.service';
   imports: [ImportsModule, FormsModule, DropdownModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
+  providers: [MessageService, ConfirmationService],
 })
 export class ContactComponent implements OnInit {
   contactForm: FormGroup = new FormGroup({});
   constructor(
-    private commonService: CommonService
+    private commonService: CommonService,
+    private messageService: MessageService
   ) {}
 
   ngOnInit() {
@@ -51,7 +54,23 @@ export class ContactComponent implements OnInit {
     };
 
     this.commonService.contactSubmit(payload).subscribe((data) => {
-      if (data) {
+      if (data.status === 200) {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'success',
+          detail:
+            data.error.text ??
+            'Your information has been submitted successfully and we will get back to you soon.',
+        });
+        this.contactForm.reset();
+      } else {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail:
+            data.error.errorMessage ??
+            'The application has encountered an unknown error. Please try again later.',
+        });
         this.contactForm.reset();
       }
     });
